13ac8cb77b4988d5bb239867e4226be4abffc301
github username <github email address>
2019-02-05 13:07:37 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :clickbait, unless: Proc.new { |a| a.title.blank? }
+
+  def clickbait
+    bait_words = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+    truth = 0
+    bait_words.each do |word|
+      if self.title.include?(word)
+        truth += 1
+      end
+    end
+    if truth == 0
+      errors.add(:title, "must be clickbait")
+    end
+  end
 end
